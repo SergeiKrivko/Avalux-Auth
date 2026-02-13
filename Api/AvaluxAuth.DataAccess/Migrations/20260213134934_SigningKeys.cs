99679e05c640f59e3ead891c3cb17a84b6ef8195
SergeiKrivko <sergik.krivko@gmail.com>
2026-02-13 21:28:41 +0300

    migration

---
+++ Api/AvaluxAuth.DataAccess/Migrations/20260213134934_SigningKeys.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvaluxAuth.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SigningKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SigningKeys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Kid = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Algorithm = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Use = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    PrivateKeyEncrypted = table.Column<string>(type: "character varying(8000)", maxLength: 8000, nullable: false),
+                    PublicJwkJson = table.Column<string>(type: "character varying(8000)", maxLength: 8000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SigningKeys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SigningKeys");
+        }
+    }
+}
