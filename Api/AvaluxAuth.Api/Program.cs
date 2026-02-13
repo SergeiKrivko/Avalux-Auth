2c535145001254778187a55e6cfa0e136710fdc5
SergeiKrivko <sergik.krivko@gmail.com>
2026-02-13 22:05:07 +0300

    rotate signing key on start

--- Api/AvaluxAuth.Api/Program.cs
+++ Api/AvaluxAuth.Api/Program.cs
@@ -93,6 +93,7 @@ if (app.Environment.IsDevelopment())
 await using (var scope = app.Services.CreateAsyncScope())
 {
     await scope.ServiceProvider.GetRequiredService<AvaluxAuthDbContext>().Database.MigrateAsync();
+    await scope.ServiceProvider.GetRequiredService<ISigningKeyService>().RotateSigningKeyAsync();
 }
 
 app.Run();
