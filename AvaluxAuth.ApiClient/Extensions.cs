ca611d71f228541550301ca7d93fbb826689f690
SergeiKrivko <sergik.krivko@gmail.com>
2026-03-17 22:47:54 +0300

    services.AddAvaluxAuthApiClient()

---
+++ AvaluxAuth.ApiClient/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avalux.Auth.ApiClient;
+
+public static class Extensions
+{
+    public static IServiceCollection AddAvaluxAuthApiClient(this IServiceCollection services, string apiUrl,
+        string apiKey)
+    {
+        services.AddHttpClient("AvaluxAuth", options =>
+        {
+            options.BaseAddress = new Uri(apiUrl);
+            options.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+        });
+
+        services.AddScoped<IAuthClient, HttpClientFactoryAuthClient>();
+
+        return services;
+    }
+
+    private class HttpClientFactoryAuthClient(IHttpClientFactory httpClientFactory)
+        : AuthClient(httpClientFactory.CreateClient("AvaluxAuth"));
+}
